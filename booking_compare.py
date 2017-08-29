45db12c401c2b0c4ff76afed2f75d72bd0503a39
Qiman Wang <wangqiman0111@gmail.com>
2017-08-29 11:33:42 -0400

    added booking_compare v2

--- booking_compare.py
+++ booking_compare.py
@@ -13,13 +13,52 @@ for f in files:
         ok_file = f
     if " cancel " in f and f[:2] != "~$":
         cancel_file = f
-    if "rstatement" in f and f[:2] != "~$":
-
+    if "Booking.com" in f and f[:2] != "~$":
         booking_file = f
 
 msg = "Files detected:\n" + booking_file + "\n" + ok_file + "\n" + cancel_file + "\n" + "Press Enter key to begin the process."
-a = input(msg)
+a = raw_input(msg)
 
+
+# developed with python 3.5 for pyinstaller function
+#############################################################
+# check conditions and print out
+def same_name(booking_name, hotel_name):
+    if hotel_name[:5] == 'name\n':
+        hotel_name = hotel_name[5:]
+
+    hotel_name = hotel_name.split(",")
+    hotel_name = hotel_name[1][1:] + " " + hotel_name[0]
+
+    if hotel_name == booking_name:
+        # print("h:", hotel_name, "b:", booking_name)
+        return True
+    else:
+        return False
+
+
+def calc_hotel_price(hotel_price, arrival, departure):
+    # print("input: ", booking_price, hotel_price, arrival, departure)
+
+    if departure[:2] == "De":
+        departure = departure[9:]
+        arrival = arrival[7:]
+        hotel_price = hotel_price[4:]
+
+    date_format = "%m/%d/%Y"
+    a = datetime.strptime(departure, date_format)
+    b = datetime.strptime(arrival, date_format)
+    delta = a - b
+    # print("hotel price: ", float(delta.days) * float(hotel_price), "booking price: ", float(booking_price))
+    # print(float(delta.days) * float(hotel_price) == float(booking_price))
+    return (float(delta.days) * float(hotel_price))
+
+
+######################################################################################
+match = 0
+diff_price = 0
+canceled = 0
+not_found = 0
 
 # load and prepare the booking df
 xl = pd.ExcelFile(booking_file)
@@ -35,6 +74,7 @@ for index, row in b_df.iterrows():
 # loading the ok DF
 xl = pd.ExcelFile(ok_file)
 ok_df = xl.parse("Sheet1", header=2, keep_default_na=False, )
+print(list(ok_df))
 o_df = ok_df.loc[(ok_df['Name'] != "") & (ok_df['Name'] != "Name") &
                  (ok_df['Name'] != "Guest List Detail") &
                  (ok_df['Name'] != "30031 - DAYS INN PHILADELPHIA.") &
@@ -57,6 +97,7 @@ o_df = o_df.loc[(o_df['Rate'] != "")]
 # loading the cancel DF
 xl = pd.ExcelFile(cancel_file)
 c_df = xl.parse("Sheet1", header=2, keep_default_na=False, )
+print(list(c_df))
 c_df = c_df.loc[(c_df['Name'] != "") & (c_df['Name'] != "Name") &
                 (c_df['Name'] != "Guest List Detail") &
                 (c_df['Name'] != "30031 - DAYS INN PHILADELPHIA.") &
@@ -74,58 +115,40 @@ c_df = c_df.loc[(c_df['Rate'] != "")]
 # for index, row in c_df.iterrows():
 #     print("index:::", index, row['Name'], row['Arrival'], row['Departure'], row['Rate'])
 
+ok_file = ""
+cancel_file = ""
+booking_file = ""
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+for f in files:
+    if " ok " in f and f[:2] != "~$":
+        ok_file = f
+    if " cancel " in f and f[:2] != "~$":
+        cancel_file = f
+    if " booking " in f and f[:2] != "~$":
+        booking_file = f
 
 #########################################
 print("begin comparison")
-workbook = xlsxwriter.Workbook('Expenses01.xlsx')
+# create worksheet
+workbook = xlsxwriter.Workbook('Expenses.xlsx')
 worksheet = workbook.add_worksheet()
 rowm = 0
 coln = 0
 worksheet.write(rowm, coln, "Name")
 worksheet.write(rowm, coln + 1, "Booking.com Price")
-worksheet.write(rowm, coln + 2, "Discritpion")
+worksheet.write(rowm, coln + 2, "Hotel Price")
+worksheet.write(rowm, coln + 3, "Price difference")
+worksheet.write(rowm, coln + 4, "Discritpion")
 rowm += 1
 
-
+workbook_nf = xlsxwriter.Workbook('Customers Not found.xlsx')
+worksheet_nf = workbook_nf.add_worksheet()
+row_nf = 0
 # # print b_df
 # print(list(b_df))
 # for index, row in b_df.iterrows():
 #     print("index:::", index, row['Guest name(s)'].lower(),row['Check-in'],row['Check-out'],row['Price'])
 #
-
-#############################################################
-# check conditions and print out
-def same_name(booking_name, hotel_name):
-    if hotel_name[:5] == 'name\n':
-        hotel_name = hotel_name[5:]
-
-    hotel_name = hotel_name.split(",")
-    hotel_name = hotel_name[1][1:] + " " + hotel_name[0]
-
-    if hotel_name == booking_name:
-        # print("h:", hotel_name, "b:", booking_name)
-        return True
-    else:
-        return False
-
-
-def same_price(booking_price, hotel_price, arrival, departure):
-    # print("input: ", booking_price, hotel_price, arrival, departure)
-
-    if departure[:2] == "De":
-        departure = departure[9:]
-        arrival = arrival[7:]
-        hotel_price = hotel_price[4:]
-
-    date_format = "%m/%d/%Y"
-    a = datetime.strptime(departure, date_format)
-    b = datetime.strptime(arrival, date_format)
-    delta = a - b
-    # print("hotel price: ", float(delta.days) * float(hotel_price), "booking price: ", float(booking_price))
-    # print(float(delta.days) * float(hotel_price) == float(booking_price))
-    if (float(delta.days) * float(hotel_price) == float(booking_price)):
-        return True
-
 
 # check loop
 for index, row in b_df.iterrows():
@@ -163,27 +186,77 @@ for index, row in b_df.iterrows():
 
             if (h_arrival == b_checkin) & (h_departure == b_checkout):
                 found = True
-                print()
-                if not same_price(price, row_ok['Rate'][1:], row_ok['Arrival'], row_ok['Departure']):
+                hotel_price = calc_hotel_price(row_ok['Rate'][1:], row_ok['Arrival'], row_ok['Departure'])
+                price_difference = hotel_price - float(price)
+                if price_difference != 0.0:
                     print(name, price, "found in Ok file ,but different price")
                     worksheet.write(rowm, coln, name)
                     worksheet.write(rowm, coln + 1, price)
-                    worksheet.write(rowm, coln + 2, "Found in OK file, but price is different.")
+                    worksheet.write(rowm, coln + 2, hotel_price)
+                    worksheet.write(rowm, coln + 3, price_difference)
+                    worksheet.write(rowm, coln + 4, "Found in OK file, but price is different.")
                     rowm += 1
-                    break
+                    diff_price += 1
+                else:
+                    match += 1
+                    print(name, ' Everything match')
+
+            break
 
     if (found == False):
         # check if in canceled
         for index_c, row_c in c_df.iterrows():
             if (same_name(name.lower(), row_c['Name'].lower())):
-                print(name + "**BAD***********************************************")
-                worksheet.write(rowm, coln, name)
-                worksheet.write(rowm, coln + 2, "Found in Cancel file.")
-                rowm += 1
-                found = True
-                break
+                # print(name + "**BAD***********************************************")
+                # worksheet.write(rowm, coln, name)
+                #
+                # worksheet.write(rowm, coln + 4, "Found in Cancel file.")
+                # rowm += 1
+                # found = True
+                # canceled += 1
+
+                if row_c['Departure'][:2] == "De":
+                    departure = row_c['Departure'][9:]
+                    arrival = row_c['Arrival'][7:]
+                else:
+                    departure = row_c['Departure']
+                    arrival = row_c['Arrival']
+                    # hotel date
+                date_format = "%m/%d/%Y"
+                h_departure = datetime.strptime(departure, date_format)
+                h_arrival = datetime.strptime(arrival, date_format)
+
+                # booking.com date
+
+                b_checkin = datetime(int(check_in_date[0]), int(check_in_date[1]), int(check_in_date[2]))
+                b_checkout = datetime(int(check_out_date[0]), int(check_out_date[1]), int(check_out_date[2]))
+
+                if (h_arrival == b_checkin) & (h_departure == b_checkout):
+                    found = True
+                    hotel_price = calc_hotel_price(row_ok['Rate'][1:], row_ok['Arrival'], row_ok['Departure'])
+                    price_difference = hotel_price - float(price)
+
+                    print(name, price, "found in CANCEL file" )
+                    worksheet.write(rowm, coln, name)
+                    worksheet.write(rowm, coln + 1, price)
+                    worksheet.write(rowm, coln + 2, hotel_price)
+                    worksheet.write(rowm, coln + 3, price_difference)
+                    worksheet.write(rowm, coln + 4, "found in CANCEL file, but price is different.")
+                    rowm += 1
+                    diff_price += 1
+
+                    break
 
     if found == False:
         print(name, " Can't find customer Name")
+        not_found += 1
+        # workbook_nf.write(row_nf,0,name)
+        # row_nf += 1
 
 workbook.close()
+workbook_nf.close()
+
+print("match: ", match)
+print("diff_price: ", diff_price)
+print("canceled: ", canceled)
+print("not found: ", not_found)
---
+++ booking_compare.spec
@@ -0,0 +1,33 @@
+# -*- mode: python -*-
+
+block_cipher = None
+
+
+a = Analysis(['booking_compare.py'],
+             pathex=['C:\\Users\\qiman\\Desktop\\Github\\Joyce_stuff\\Joyce'],
+             binaries=[],
+             datas=[],
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          exclude_binaries=True,
+          name='booking_compare',
+          debug=False,
+          strip=False,
+          upx=True,
+          console=True )
+coll = COLLECT(exe,
+               a.binaries,
+               a.zipfiles,
+               a.datas,
+               strip=False,
+               upx=True,
+               name='booking_compare')
